3579253a9a12dcff511e0668909173aa7cf328d6
irfanizudin <irfanizzudyn@gmail.com>
2022-05-04 19:57:04 +0700

    Create extension UIView file

---
+++ Swiftagram/Resources/Extension.swift
@@ -0,0 +1,34 @@
+//
+//  Extension.swift
+//  Swiftagram
+//
+//  Created by Irfan Izudin on 04/05/22.
+//
+
+import UIKit
+
+extension UIView {
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+}
