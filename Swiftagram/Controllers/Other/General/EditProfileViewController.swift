9b22c0e6ea3751247d41c0fb2d6b7d60043eff73
irfanizudin <irfanizzudyn@gmail.com>
2022-05-07 21:06:26 +0700

    Create EditProfileViewController

--- Swiftagram/Controllers/Other/General/EditProfileViewController.swift
+++ Swiftagram/Controllers/Other/General/EditProfileViewController.swift
@@ -7,12 +7,120 @@
 
 import UIKit
 
-class EditProfileViewController: UIViewController {
+struct EditProfileFormModel {
+    let label: String
+    let placeholder: String
+    let value: String?
+}
 
+class EditProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private var models = [[EditProfileFormModel]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureModel()
+        tableView.tableHeaderView = createTableHeaderView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
         view.backgroundColor = .systemBackground
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSave))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(didTapCancel))
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    private func configureModel(){
+        let sectionLabel1 = ["Name", "Username", "Bio"]
+        var section1 = [EditProfileFormModel]()
+        for label in sectionLabel1 {
+            let model = EditProfileFormModel(label: label, placeholder: "Enter \(label)...", value: nil)
+            section1.append(model)
+        }
+        models.append(section1)
         
+        let sectionLabel2 = ["Email", "Phone", "Gender"]
+        var section2 = [EditProfileFormModel]()
+        for label in sectionLabel2 {
+            let model = EditProfileFormModel(label: label, placeholder: "Enter \(label)...", value: nil)
+            section2.append(model)
+        }
+        models.append(section2)
+        
+    }
+    
+    private func createTableHeaderView() -> UIView {
+        let header = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: view.height/3))
+        let size = header.height/1.5
+        let profilePhotoButton = UIButton(frame: CGRect(x: (view.width-size)/2, y: (header.height-size)/2, width: size, height: size))
+        header.addSubview(profilePhotoButton)
+        profilePhotoButton.layer.masksToBounds = true
+        profilePhotoButton.layer.cornerRadius = size/2
+        profilePhotoButton.addTarget(self, action: #selector(didTapProfilePhotoButton), for: .touchUpInside)
+        profilePhotoButton.setBackgroundImage(UIImage(systemName: "person.circle"), for: .normal)
+        profilePhotoButton.layer.borderWidth = 1
+        profilePhotoButton.layer.borderColor = UIColor.secondarySystemBackground.cgColor
+        return header
+    }
+
+    @objc private func didTapProfilePhotoButton(){
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let model = models[indexPath.section][indexPath.row]
+        cell.textLabel?.text = model.label
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard section == 1 else {
+            return nil
+        }
+        return "Private Information"
+    }
+    
+    @objc private func didTapSave(){
+        dismiss(animated: true)
+    }
+    
+    @objc private func didTapCancel(){
+        dismiss(animated: true)
+    }
+    
+    @objc private func didTapChangeProfilePicture(){
+        let actionSheet = UIAlertController(title: "Profile Picture", message: "Change profile picture", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { _ in
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Choose From Library", style: .default, handler: { _ in
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(actionSheet, animated: true)
     }
 
 }
